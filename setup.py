1e5196c03e304f68d093456a467ebe428e8817dd
Stephen Kuenzli <skuenzli@gmail.com>
2013-05-08 15:45:00 -0700

    Release version 0.1.3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import multiprocessing
 
 setup(
     name='time-series-analysis',
-    version='0.1.3-dev',
+    version='0.1.3',
     description='Contains tools for analyzing time-series data on the command-line.',
     url='https://github.com/skuenzli/time-series-analysis',
     author='Stephen Kuenzli',
